b113bf0913177a280418d374cd623a87eb27cc9d
0xInu <calin.cretiu@gmail.com>
2022-06-06 08:46:19 +0300

    MoralisProvider, web3uikit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { MoralisProvider } from "react-moralis";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId="3zvmZ7CBMV1cVw9mIPnrtUiPYZHuHKkaVdDVNAVn"
+      serverUrl="https://bexnvkjunxyf.usemoralis.com:2053/server"
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
 );
 
